5cfd7fd85ba9c48f7463fd21e805a8ed6ba8bce2
Dranoel2 <45979197+Dranoel2@users.noreply.github.com>
2022-03-13 10:25:44 +0000

    Add pnpm version

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-	import Footer from "$lib/components/Footer.svelte";
-  import Navbar from "$lib/components/navbar/Navbar.svelte";
-	import "../app.css";
+	import Footer from '$lib/components/Footer.svelte';
+	import Navbar from '$lib/components/navbar/Navbar.svelte';
+	import '../app.css';
 </script>
 
 <div class="p-3 h-full">
-  <div class="w-full">
-    <Navbar />
+	<div class="w-full">
+		<Navbar />
 
-    <main id="main">
-      <slot />
-    </main>
-  </div>
+		<main id="main">
+			<slot />
+		</main>
+	</div>
 
-  <Footer />
+	<Footer />
 </div>
